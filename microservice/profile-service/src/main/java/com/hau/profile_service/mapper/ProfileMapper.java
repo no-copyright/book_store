e50e17d19dd4e00920797974cccbfe4888705009
datdnit <datdnk3@gmail.com>
2025-04-12 22:02:14 +0700

    update profile service

--- microservice/profile-service/src/main/java/com/hau/profile_service/mapper/ProfileMapper.java
+++ microservice/profile-service/src/main/java/com/hau/profile_service/mapper/ProfileMapper.java
@@ -2,13 +2,17 @@ package com.hau.profile_service.mapper;
 
 import com.hau.profile_service.dto.ProfileCreateRequest;
 import com.hau.profile_service.dto.ProfileResponse;
+import com.hau.profile_service.dto.ProfileUpdateRequest;
 import com.hau.profile_service.entity.Profile;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface ProfileMapper {
     Profile toUserProfile(ProfileCreateRequest profileCreateRequest);
+
+    void updateUserProfile(@MappingTarget Profile profile, ProfileUpdateRequest profileUpdateRequest);
 
     ProfileResponse toUserProfileResponse(Profile profile);
 }
--- microservice/profile-service/src/main/java/com/hau/profile_service/service/ProfileService.java
+++ microservice/profile-service/src/main/java/com/hau/profile_service/service/ProfileService.java
@@ -3,6 +3,7 @@ package com.hau.profile_service.service;
 import com.hau.profile_service.dto.ApiResponse;
 import com.hau.profile_service.dto.ProfileCreateRequest;
 import com.hau.profile_service.dto.ProfileResponse;
+import com.hau.profile_service.dto.ProfileUpdateRequest;
 import com.hau.profile_service.entity.Profile;
 import com.hau.profile_service.exception.AppException;
 import com.hau.profile_service.mapper.ProfileMapper;
@@ -19,26 +20,54 @@ public class ProfileService {
     private final ProfileRepository profileRepository;
     private final ProfileMapper profileMapper;
 
-    public ApiResponse<ProfileResponse> createUserProfile(ProfileCreateRequest profileCreateRequest) {
+    public ApiResponse<ProfileResponse> createProfile(ProfileCreateRequest profileCreateRequest) {
         Profile profile = profileMapper.toUserProfile(profileCreateRequest);
 
         profileRepository.save(profile);
 
         return ApiResponse.<ProfileResponse>builder()
                 .status(HttpStatus.CREATED.value())
-                .message("Tạo mới thông tin user thành công")
+                .message("Thêm thông tin người nhận hàng thành công")
                 .result(profileMapper.toUserProfileResponse(profile))
                 .timestamp(LocalDateTime.now())
                 .build();
     }
 
-    public ApiResponse<ProfileResponse> getUserProfileById(Long userId) {
-        Profile profile = profileRepository.findById(userId)
-                .orElseThrow(() -> new AppException(HttpStatus.BAD_REQUEST, "Không tìm thấy thông tin user", null));
+
+    public ApiResponse<ProfileResponse> getProfileById(Long profileId) {
+        Profile profile = profileRepository.findById(profileId)
+                .orElseThrow(() -> new AppException(HttpStatus.BAD_REQUEST, "Không tìm thấy thông tin", null));
         return ApiResponse.<ProfileResponse>builder()
                 .status(HttpStatus.OK.value())
-                .message("Lấy thông tin user thành công")
+                .message("Lấy thông tin thành công")
                 .result(profileMapper.toUserProfileResponse(profile))
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+
+    public ApiResponse<ProfileResponse> updateProfile(Long userId, ProfileUpdateRequest profileUpdateRequest) {
+        Profile profile = profileRepository.findById(userId)
+                .orElseThrow(() -> new AppException(HttpStatus.BAD_REQUEST, "Không tìm thấy thông tin", null));
+        profileMapper.updateUserProfile(profile, profileUpdateRequest);
+        profileRepository.save(profile);
+
+        return ApiResponse.<ProfileResponse>builder()
+                .status(HttpStatus.OK.value())
+                .message("Cập nhật thông tin thành công")
+                .result(profileMapper.toUserProfileResponse(profile))
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+
+    public ApiResponse<String> deleteProfile(Long profileId) {
+        Profile profile = profileRepository.findById(profileId)
+                .orElseThrow(() -> new AppException(HttpStatus.BAD_REQUEST, "Không tìm thấy thông tin", null));
+        profileRepository.delete(profile);
+
+        return ApiResponse.<String>builder()
+                .status(HttpStatus.OK.value())
+                .message("Xóa thông tin thành công")
+                .result(null)
                 .timestamp(LocalDateTime.now())
                 .build();
     }
