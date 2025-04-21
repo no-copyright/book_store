01af733bd3e995fce52b20a9d594c322e541551a
lmao1166 <tuantt3010@gmail.com>
2025-04-21 17:02:43 +0700

    check

--- microservice/product-service/src/main/java/com/hau/productservice/controller/ProductController.java
+++ microservice/product-service/src/main/java/com/hau/productservice/controller/ProductController.java
@@ -1,13 +1,14 @@
 package com.hau.productservice.controller;
 
+import com.hau.productservice.dto.request.ProductRequest;
+import com.hau.productservice.dto.response.ApiResponse;
+import com.hau.productservice.dto.response.ProductResponse;
 import com.hau.productservice.entity.Product;
 import com.hau.productservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +22,11 @@ public class ProductController {
     public List<Product> getAllProduct(@RequestParam(required = false) Integer pageIndex,
                                        @RequestParam(required = false) Integer pageSize) {
         return productService.getAllProduct(pageIndex, pageSize);
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<ProductResponse>> addProduct(@RequestBody ProductRequest request) {
+        ApiResponse<ProductResponse> response = productService.createProduct(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 }
--- microservice/product-service/src/main/java/com/hau/productservice/service/ProductService.java
+++ microservice/product-service/src/main/java/com/hau/productservice/service/ProductService.java
@@ -1,22 +1,41 @@
 package com.hau.productservice.service;
 
+import com.hau.productservice.dto.request.ProductRequest;
 import com.hau.productservice.dto.response.ApiResponse;
 import com.hau.productservice.dto.response.ProductResponse;
 import com.hau.productservice.entity.Product;
+import com.hau.productservice.mapper.ProductMapper;
 import com.hau.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.awt.print.Pageable;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+    public ApiResponse<ProductResponse> createProduct(ProductRequest request) {
+        Product product = productMapper.toProduct(request);
+        Product savedProduct = productRepository.save(product);
+        ProductResponse productResponse = productMapper.toProductResponse(savedProduct);
+        return ApiResponse.<ProductResponse>builder()
+                .status(HttpStatus.CREATED.value())
+                .message("Thêm sản phẩm thành công")
+                .result(productResponse)
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
 
     public List<Product> getAllProduct(Integer pageIndex, Integer pageSize) {
         List<Product> products = productRepository.findAll();
         return products;
     }
+
+
+
 }
