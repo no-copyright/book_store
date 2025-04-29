92f0ecbe04bd50addc9d61c064064cd4b46987d9
lmao1166 <tuantt3010@gmail.com>
2025-04-30 00:23:15 +0700

    import cleaning

--- microservice/blog-service/src/main/java/com/hau/blogService/repository/CategoryRepository.java
+++ microservice/blog-service/src/main/java/com/hau/blogService/repository/CategoryRepository.java
@@ -4,7 +4,6 @@ import com.hau.blogService.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
